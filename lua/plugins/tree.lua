fa61f25afbbdc2c257ae1290e5b0426ad104b3b1
marlonmp <76027579+marlonmp@users.noreply.github.com>
2026-08-10 17:23:44 -0500

    tree visual changes added

--- lua/plugins/tree.lua
+++ lua/plugins/tree.lua
@@ -2,7 +2,22 @@ return {
     "nvim-tree/nvim-tree.lua",
     dependencies = { "nvim-tree/nvim-web-devicons" },
     config = function()
-        require("nvim-tree").setup()
+        require("nvim-tree").setup({
+            view = {
+                number = true,         -- enables line numbers
+                relativenumber = true, -- makes them relative
+            },
+            renderer = {
+                root_folder_label = function(path)
+                    return "[" .. vim.fn.fnamemodify(path, ":t") .. "]"
+                end,
+            },
+            actions = {
+                open_file = {
+                    quit_on_open = true, -- closes the tree window after opening a file
+                },
+            },
+        })
         vim.keymap.set("n", "<leader>e", ":NvimTreeToggle<CR>", { silent = true })
     end,
 }
