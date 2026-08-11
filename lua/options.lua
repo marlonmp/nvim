0a744b87317273aff438fdfa07339306b380beaa
marlonmp <76027579+marlonmp@users.noreply.github.com>
2026-08-11 10:48:39 -0500

    whitespace rendering added

--- lua/options.lua
+++ lua/options.lua
@@ -14,6 +14,16 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 vim.opt.termguicolors = true
 
+-- enable whitespace rendering
+vim.opt.list = true
+vim.opt.listchars = {
+  space = '·',
+  tab = '→ ',
+  trail = '•',
+  -- eol = '↲',
+  nbsp = '␣',
+}
+
 -- undo tree
 vim.opt.undofile = true
 vim.opt.undodir = vim.fn.stdpath("cache") .. "/undo"
@@ -27,9 +37,9 @@ vim.api.nvim_create_autocmd("FileType", {
     group = indent_group,
     callback = function()
         vim.opt_local.expandtab = false
-        vim.opt_local.shiftwidth = 2
-        vim.opt_local.tabstop = 2
-        vim.opt_local.softtabstop = 2
+        vim.opt_local.shiftwidth = 4
+        vim.opt_local.tabstop = 4
+        vim.opt_local.softtabstop = 4
     end,
 })
 
