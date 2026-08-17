155df8de6f2f633cc323c282e5bfda5e307d0f5f
marlonmp <76027579+marlonmp@users.noreply.github.com>
2026-08-17 17:56:47 -0500

    go to implementation keymap added

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -55,6 +55,7 @@ return {
         -- Global keymaps for interacting with language features
         vim.keymap.set("n", "K", vim.lsp.buf.hover, { desc = "Show documentation" })
         vim.keymap.set("n", "gd", vim.lsp.buf.definition, { desc = "Go to definition" })
+        vim.keymap.set("n", "gi", vim.lsp.buf.implementation, { desc = "Go to implementation" })
         vim.keymap.set("n", "gr", vim.lsp.buf.references, { desc = "Go to references" })
         vim.keymap.set("n", "<leader>ca", vim.lsp.buf.code_action, { desc = "Code actions" })
         vim.keymap.set("n", "<leader>rn", vim.lsp.buf.rename, { desc = "Rename symbol" })
